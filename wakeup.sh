bfa48894f24cd050236b8748e19d94eac84100c8
Dora F <doradora.prog@gmail.com>
2021-11-13 11:20:01 +0900

    Fix discord_notify_url

--- wakeup.sh
+++ wakeup.sh
@@ -1,3 +1,4 @@
-curl -H "Content-Type: application/json" -X POST -d '{"username": "AzureWatcher", "content": "Restart azure"}' https://discord.com/api/webhooks/908894969073385512/TeKXr1Yv-BBQ-SxHgb7iJHTRMzHba83jLBOXCin8maQLX3aMob8nmLAbyWHPd4gN8qtR
+curl -H "Content-Type: application/json" -X POST -d '{"username": "AzureWatcher", "content": "Restart azure"}' $AZURE_NOTIFY_URL
+echo $AZURE_NOTIFY_URL
 cd /server/azure
 ./azure > ./azlog.log
