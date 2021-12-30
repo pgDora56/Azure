6e880c1ba7c0b10dbc80e54c42c213aa4e009e64
Dora F <doradora.prog@gmail.com>
2021-12-30 11:29:14 +0900

    Change wakeup shell script

--- wakeup.sh
+++ wakeup.sh
@@ -1,4 +1,8 @@
 curl -H "Content-Type: application/json" -X POST -d '{"username": "AzureWatcher", "content": "Restart azure"}' $AZURE_NOTIFY_URL
 echo $AZURE_NOTIFY_URL
 cd /server/azure
-./azure > ./azlog.log
+outfile=`date '+logs/azoutlog%Y%m%d%H%M%S.log'`
+errfile=`date '+logs/azerrlog%Y%m%d%H%M%S.log'`
+mv azoutlog.log $outfile
+mv azerrlog.log $errfile
+./azure 1> ./azoutlog.log 2> ./azerrlog.log
